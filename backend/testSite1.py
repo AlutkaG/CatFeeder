37f627ed64e1689cc62b8ee2e74a737cd4bae12f
AlutkaG <alagerlach@interia.pl>
2020-08-11 11:04:38 +0200

    Change route method

--- backend/testSite1.py
+++ backend/testSite1.py
@@ -1,9 +1,10 @@
+import time
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/hello")
-def index():
-  return "<html><body><h1>Test site running under Flask</h1></body></html>"
+@app.route("/time")
+def get_current_time():
+  return {'time': time.time()}
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
