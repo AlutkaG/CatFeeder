6daf45f3d0c7ca2a058ff59c1ea1f73a03e90dde
AlutkaG <alagerlach@interia.pl>
2020-08-11 10:24:25 +0200

    Append fetch method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+	const [ala, setAla] = useState("");
+
+	useEffect(() => {
+		fetch("192.168.1.22:8000")
+			.then((res) => res.json())
+			.then((data) => {
+				setAla(data.body);
+			});
+	}, []);
 	return (
 		<div className='App'>
 			<Navbar />
+			{ala}
 		</div>
 	);
 }
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+	FaHome,
+	FaPaw,
+	FaTemperatureLow,
+	FaClock,
+	FaVideo,
+	FaQuestion,
+	FaCog,
+} from "react-icons/fa";
 
 import Aux from "../../hoc/Aux";
 
@@ -10,11 +19,45 @@ const navbar = (props) => {
 			<div className='sideNav'>
 				<p className='appName'>Pet Feeder</p>
 				<div className='sideNavLinks'>
-					<a href='/'>Home</a>
-					<a href='/'>My pets</a>
-					<a href='/'>Dose</a>
-					<a href='/'>Camera</a>
-					<a href='/'>Settings</a>
+					<a href='/'>
+						<FaHome style={{ paddingRight: "20px", color: " #e0afa0" }} />
+						Home
+					</a>
+					<a href='/'>
+						<FaPaw style={{ paddingRight: "20px", color: " #e0afa0" }} />
+						My pets
+					</a>
+					<a href='/'>
+						<FaTemperatureLow
+							style={{ paddingRight: "20px", color: " #e0afa0" }}
+						/>
+						Temperature
+					</a>
+					<a href='/'>
+						<FaClock style={{ paddingRight: "20px", color: " #e0afa0" }} />
+						Dose
+					</a>
+					<a href='/'>
+						<FaVideo style={{ paddingRight: "20px", color: " #e0afa0" }} />
+						Camera
+					</a>
+					<a href='/'>
+						<FaQuestion style={{ paddingRight: "20px", color: " #e0afa0" }} />{" "}
+						About app
+					</a>
+					<a href='/'>
+						<FaCog style={{ paddingRight: "20px", color: " #e0afa0" }} />
+						Settings
+					</a>
+				</div>
+				<div className='footerNav'>
+					©{new Date().getFullYear()} <strong>Pet Feeder</strong>
+					<br />
+					<div className='footerLink'>
+						<a href='/' style={{ textDecoration: "none", color: "white" }}>
+							Contact
+						</a>
+					</div>
 				</div>
 			</div>
 		</Aux>
