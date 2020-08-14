8bc72e2781561102d02674cd0e4cdbee43cb5377
AlutkaG <alagerlach@interia.pl>
2020-08-14 22:18:21 +0200

    Append of new route to take temperature

--- backend/testSite1.py
+++ backend/testSite1.py
@@ -1,10 +1,21 @@
 import time
+import w1thermsensor
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/api", methods=['GET'])
-def api():
-  return {'userId': 1, 'title': 'Flask React Application', 'completed': False}
+app = Flask(__name__)
+#app.config['CORS_HEADERS'] = 'Content-Type'
+#cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+sensor = w1thermsensor.W1ThermSensor()
+
+@app.route("/api/v1/time")
+def get_current_time():
+  return {'time': time.time()}
+
+@app.route("/api/v1/temp")
+def get_current_temp():
+  temp = sensor.get_temperature()
+  return {'temp': temp}
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
