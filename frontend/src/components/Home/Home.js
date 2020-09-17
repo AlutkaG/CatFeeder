88f4872d0f1fbb1fb27b1cae49201f00b9e863eb
AlutkaG <alagerlach@interia.pl>
2020-09-17 19:09:55 +0200

    Change style

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,21 +2,35 @@ import React from "react";
 
 import Aux from "../../hoc/Aux";
 
-import pets from "../../img/pets_home.png";
+import feed from "../../img/homeColor.png";
 
 import "./Home.css";
 
 const home = (props) => {
 	return (
 		<Aux>
-			<div className='background'>
-				<div className='column left'>
-					<img width='1000px' src={pets} alt={"Pets"} />
+			<div className='column right'>
+				<img
+					src={feed}
+					alt='Pets'
+					width='750px'
+					style={{
+						boxShadow: "10px 10px 75px 30px rgba(0, 0, 0, 0.64)",
+						borderRadius: "100%",
+					}}
+				></img>
+			</div>
+			<div className='column left'>
+				<div className='textBox1'>
+					<div className='text1'>Are your pet hungry?</div>
 				</div>
-				<div className='column right'>
-					<div className='pressText'>Press to feed:</div>
-
-					<button className='pressButton' />
+				<button className='pressButton' />
+			</div>
+			<div className='column center'>
+				<div className='textBox2'>
+					<div className='text2'>
+						You can feed your pet by clicking on the button!
+					</div>
 				</div>
 			</div>
 		</Aux>
