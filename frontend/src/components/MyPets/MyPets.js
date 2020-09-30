841bba4dc460d8b9c9a4b19aae46a7d74a3bc144
AlutkaG <alagerlach@interia.pl>
2020-09-30 11:02:42 +0200

    Added sumbit method to post data from form to database

--- frontend/src/components/MyPets/MyPets.js
+++ frontend/src/components/MyPets/MyPets.js
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Aux from "../../hoc/Aux";
 
 import "./MyPets.css";
+import Axios from "axios";
 
 function MyPets() {
+	const [name, setName] = useState("");
+	const [type, setType] = useState("");
+	const [portion, setPortion] = useState("");
+	const [hours, setHours] = useState("");
+	const [minutes, setMinutes] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		const formData = new FormData();
+		formData.append("name", name);
+		formData.append("type", type);
+		formData.append("portion", portion);
+		formData.append("hours", hours);
+		formData.append("minutes", minutes);
+		let data = {
+			name: name,
+			type: type,
+			portion: portion,
+			hours: hours,
+			minutes: minutes,
+		};
+		Axios.post("http://catfeeder.ddns.net/api/v1/addpet", data)
+			.then((res) => {
+				console.log(res);
+				console.log(res.data);
+			})
+			.catch((error) => {
+				console.log(error.response);
+			});
+	};
+
 	return (
 		<Aux>
 			<div className='column left2'>
 				<div className='boxForm'>
 					<div className='addPetText'>Add Pet</div>
-					<form className='formAddPet'>
+					<form className='formAddPet' onSubmit={handleSubmit}>
 						<label>
 							Name: <span />
 							<input
 								type='text'
 								placeholder='Name...'
+								name='name'
+								onChange={(e) => setName(e.target.value)}
 								style={{ marginLeft: "6%", marginBottom: "4%" }}
 							/>
 						</label>
@@ -25,6 +60,8 @@ function MyPets() {
 							<input
 								type='text'
 								placeholder='Type...'
+								name='type'
+								onChange={(e) => setType(e.target.value)}
 								style={{ marginLeft: "8%", marginBottom: "4%" }}
 							/>
 						</label>
@@ -34,6 +71,8 @@ function MyPets() {
 							<input
 								type='text'
 								placeholder='Portion (1 portion -> 1 rotation of the servo)...'
+								name='portion'
+								onChange={(e) => setPortion(e.target.value)}
 								style={{ marginLeft: "7%", marginBottom: "4%" }}
 							/>
 						</label>
@@ -43,6 +82,8 @@ function MyPets() {
 							<input
 								type='text'
 								placeholder='Hour (1-24)...'
+								name='hours'
+								onChange={(e) => setHours(e.target.value)}
 								style={{ marginLeft: "7%", marginBottom: "4%" }}
 							/>
 						</label>
@@ -52,6 +93,8 @@ function MyPets() {
 							<input
 								type='text'
 								placeholder='Minutes (1-59)...'
+								name='minutes'
+								onChange={(e) => setMinutes(e.target.value)}
 								style={{ marginLeft: "1%", marginBottom: "7%" }}
 							/>
 						</label>
