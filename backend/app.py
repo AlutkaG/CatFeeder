6a9cb80bd863fe984401d4841906c96735e4175a
AlutkaG <alagerlach@interia.pl>
2020-11-17 18:12:00 +0100

    add new backend folder

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-
 import sqlite3 as sql
 from time import *
 from flask import Flask, jsonify, render_template, request
@@ -6,6 +5,7 @@ from w1thermsensor import W1ThermSensor
 import RPi.GPIO as GPIO
 from flask_crontab import Crontab
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 crontab = Crontab(app)
@@ -15,19 +15,105 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(6, GPIO.OUT) #serwo−pin 6
-GPIO.setup(25, GPIO.IN) #czujnij - jest czy nie ma karmy
+GPIO.setup(25, GPIO.IN) #czujnik - jest czy nie ma karmy
+GPIO.setup(18, GPIO.OUT) #TRIG
+GPIO.setup(23, GPIO.IN) #ECHO
 GPIO.setup(20, GPIO.OUT) #zielona  dioda
+GPIO.setup(21, GPIO.OUT) #dioda czerwona
 
 pwm = GPIO.PWM(6, 50) #czest. 50Hz
-#pwm.start(0)
 sensor = W1ThermSensor()
-
 
 def dict_factory(cursor, row):
   d = {}
   for idx, col in enumerate(cursor.description):
     d[col[0]] = row[idx]
   return d
+
+@app.route("/api/v1/register", methods=['POST'])
+def registration():
+  if request.method == "POST":
+    req = request.get_json()
+    name = req.get('name')
+    password = req.get('password')
+    q = req.get('question')
+
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT id FROM user WHERE name = ?", (str(name),))
+    exist = cur.fetchone()
+    if exist is None:
+      cur.execute("INSERT INTO user (name,password, q1) VALUES (?,?,?)",(name,password,q))
+      con.commit()
+      response = {"msg": "Registration successful"}
+    else:
+      response = {"msg": "Account is already exist"}
+    return jsonify(response)
+    con.close()
+
+@app.route("/api/v1/login", methods=['POST'])
+def login():
+  if request.method == "POST":
+    req = request.get_json()
+    name = req.get('name')
+    password = req.get('password')
+
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT password FROM user WHERE name = ?", (str(name),))
+    pswd = cur.fetchone()
+
+    if pswd is None:
+      response = {"msg": "Bad name"}
+    elif str(pswd[0]) == str(password):
+      response = {"msg": "Logged successful"}
+    else:
+      response = {"msg": "Bad password"}
+    return jsonify(response)
+    con.close()
+
+@app.route("/api/v1/forgotPassword", methods=['POST'])
+def change_pswd():
+  if request.method == "POST":
+    req = request.get_json()
+    name = req.get('name')
+    q = req.get('question')
+    pswd = req.get('password')
+
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT q1 FROM user WHERE name = ?", (str(name),))
+    q1 = cur.fetchone()
+    if q1 is None:
+      msg = {'msg': 'Bad name'}
+    elif str(q1[0]) == q:
+      cur.execute("UPDATE user SET password = ? WHERE name = ?",(pswd,name))
+      con.commit()
+      msg = {'msg': 'Change successful'}
+    else:
+      msg = {'msg': 'Bad answer'}
+    return jsonify(msg)
+    con.close()
+
+@app.route("/api/v1/displayActivePet")
+def disp_act_pet():
+  con = sql.connect("database.db")
+  cur = con.cursor()
+  cur.execute("SELECT user FROM pet WHERE active = 1")
+  user = cur.fetchone()
+  if user is None:
+    user = "0"
+  else:
+    user = str(user[0])
+  cur.execute("SELECT name FROM pet WHERE active = 1")
+  pet = cur.fetchone()
+  if pet is None:
+    pet = "0"
+  else:
+    pet = str(pet[0])
+  info = {'user': user, 'pet': pet}
+  return jsonify(info)
+  con.close()
 
 @app.route("/api/v1/time")
 def get_current_time():
@@ -40,8 +126,59 @@ def get_current_temp():
   data = {'temp': temp}
   return jsonify(data)
 
-@app.route("/api/v1/activate")
-def action():
+@app.route("/api/v1/red/<user>")
+def get_alert_no_feed(user):
+  state = 0
+  newState = GPIO.input(25)
+  if(state != newState):
+    state = newState
+  if(state==0):
+    out = {'red': '0'}
+    dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    act = "The tank is empty"
+    name = get_current_pet()
+    with sql.connect("database.db") as con:
+       cur = con.cursor()
+       cur.execute("INSERT INTO report (date,action,pet,user) VALUES (?,?,?,?)",(dateTime,act,name,user) )
+       con.commit()
+  else:
+    out = {'red': '1'}
+  return jsonify(out)
+
+@app.route("/api/v1/blue/<user>")
+def get_alert_is_pet(user):
+  while True:
+    GPIO.output(18, True) #Ustawienie stanu wysokiego na wyjscie TRIG
+    sleep(0.000001) #Wykonywanie pomiaru przez 10us
+    GPIO.output(18, False) #Ustawienie stanu niskiego na wyjscie TRIG
+    #Oczekiwanie na stan wysoki na ECHO
+    pomiar_start = time()
+    pomiar_stop = time()
+    while GPIO.input(23) == 0:
+      pomiar_start = time()
+    #Oczekiwanie na stan niski na ECHO
+    while GPIO.input(23) == 1:
+      pomiar_stop = time()
+    opoznienieSygnalu = pomiar_stop - pomiar_start
+    dystans = (opoznienieSygnalu * 34300)/2 #Wynik w [cm] 
+    sleep(1)
+    if(dystans>10 and dystans<30): #Jezeli kot jest w odleglosci pomiedzy 10cm a 30cm
+      blue = {'blue': 1}
+      _blue = 1
+      dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+      act = "The pet was nearby"
+      name = get_current_pet()
+      with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO report (date,action,pet,user) VALUES (?,?,?,?)",(dateTime,act,name,user) )
+        cur.execute("UPDATE event SET blue = 1 WHERE id = 1")
+        con.commit()
+    else:
+      blue = {'blue': 0}
+    return jsonify(blue)
+
+@app.route("/api/v1/activate/<user>")
+def action(user):
   if(GPIO.input(25) == 0 ): #Jezeli w zrobioniku jest karma
     pwm.start(0)
     try:
@@ -51,15 +188,24 @@ def action():
       pwm.ChangeDutyCycle(11.5) #wykonaj  drugi  maly  obrot w tyl
       sleep(1.5) #odczekaj  1.5 s
       GPIO.output(20,GPIO.LOW) #Wylacz  diode  zielona
+      dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+      act = "Feeding pet"
+      name = get_current_pet()
+      with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO report (date,action,pet,user) VALUES (?,?,?,?)",(dateTime,act,name,user) )
+        con.commit()
+      con.close()
     except KeyboardInterrupt:
       pwm.stop()
       GPIO.cleanup()
     return 'ok'
   else:
-    return 'pusto'
+    return jsonify(new)
 
-@app.route("/api/v1/addpet", methods = ['POST'])
-def addpet():
+
+@app.route("/api/v1/addpet/<user>", methods = ['POST'])
+def addpet(user):
   if request.method == "POST":
     req = request.get_json()
     name = req.get('name')
@@ -73,14 +219,14 @@ def addpet():
     minutes = minutes.replace("[", " ").replace("]", " ")
     with sql.connect("database.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO pet (name,type,portion,hours,minutes,active) VALUES (?,?,?,?,?,?)",(name,type,portion,hours,minutes,active) ) 
+            cur.execute("INSERT INTO pet (name,type,portion,hours,minutes,active,user) VALUES (?,?,?,?,?,?,?)",(name,type,portion,hours,minutes,active,user) ) 
             con.commit()
     return 'ok'
     con.close()
 
 
-@app.route("/api/v1/update", methods= ['POST'])
-def updatepet():
+@app.route("/api/v1/update/<user>", methods= ['POST'])
+def updatepet(user):
   if request.method == "POST":
     req = request.get_json()
     id = req.get('id')
@@ -101,8 +247,8 @@ def updatepet():
     return 'ok'
     con.close()
 
-@app.route('/api/v1/delete', methods=['POST'])
-def delete_pet():
+@app.route('/api/v1/delete/<user>', methods=['POST'])
+def delete_pet(user):
   if request.method == "POST":
     req = request.get_json()
     id = req.get('id')
@@ -115,8 +261,8 @@ def delete_pet():
     con.close()
 
 
-@app.route("/api/v1/disabled", methods = ['POST'])
-def disabled_active_pet():
+@app.route("/api/v1/disabled/<user>", methods = ['POST'])
+def disabled_active_pet(user):
   if request.method == "POST":
     req = request.get_json()
     id = req.get('idDis')
@@ -127,37 +273,37 @@ def disabled_active_pet():
     return 'ok'
     con.close()
 
-@app.route("/api/v1/enabled", methods = ['POST'])
-def enabled_active_pet():
+@app.route("/api/v1/enabled/<user>", methods = ['POST'])
+def enabled_active_pet(user):
   if request.method == "POST":
     req = request.get_json()
     id = req.get('idEn')
     with sql.connect("database.db") as con:
       cur = con.cursor()
-      cur.execute("UPDATE pet SET active = 0 WHERE id != ?", (id, ) )
+      cur.execute("UPDATE pet SET active = 0 WHERE id != ?", (id,) )
       con.commit()
       cur.execute("UPDATE pet SET active = 1 WHERE id = ?", (id, ) )
       con.commit()
     return 'ok'
     con.close()
 
-@app.route('/api/v1/list')
-def list():
+@app.route('/api/v1/list/<user>')
+def list(user):
    con = sql.connect("database.db")
    con.row_factory = dict_factory #sql.Row
    cur = con.cursor()
-   cur.execute("select * from pet")
+   cur.execute("select * from pet where user = ?", (user,))
    rows = cur.fetchall();
    return jsonify(rows)
 
-@app.route('/api/v1/getActiveId')
-def get_active_id():
+@app.route('/api/v1/getActiveId/<user>')
+def get_active_id(user):
   con = sql.connect("database.db")
   cur = con.cursor()
   cur.execute("SELECT * FROM pet")
   count = len(cur.fetchall())
   if count != 0:
-    cur.execute("SELECT id FROM pet WHERE active = 1")
+    cur.execute("SELECT id FROM pet WHERE active = 1 AND user = ?", (user,))
     id = cur.fetchone()
     if id is None:
       _id = {'id': 0}
@@ -167,6 +313,38 @@ def get_active_id():
     _id = {'id': 0}
   return jsonify(_id)
 
+@app.route('/api/v1/reportPet/<user>', methods = ['POST'])
+def get_report(user):
+  if request.method == "POST":
+    req = request.get_json()
+    name = req.get('name')
+
+    con = sql.connect("database.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("SELECT date,action FROM report WHERE pet = ? AND user = ?", (name,user) )
+    info = cur.fetchall()
+    if info is None:
+      _info = 'No report'
+    else:
+      _info = info
+    return jsonify(_info)
+
+def get_current_pet():
+  con = sql.connect("database.db")
+  cur = con.cursor()
+  cur.execute("SELECT * FROM pet")
+  count = len(cur.fetchall())
+  if count != 0:
+    cur.execute("SELECT name FROM pet WHERE active = 1")
+    name = cur.fetchone()
+    if name is None:
+      _name = "Noname"
+    else:
+      _name = str(name[0])
+  else:
+    _name = "Noname"
+  return _name
 
 def get_current_pet_hours():
   con = sql.connect("database.db")
@@ -201,12 +379,40 @@ def get_current_pet_minutes():
     min = '-1'
   return min
 
+def get_blue_value():
+  con = sql.connect("database.db")
+  cur = con.cursor()
+  cur.execute("SELECT blue FROM event WHERE id = 1")
+  _blue = cur.fetchone()
+  blue = str(_blue[0])
+  return blue
+
 @crontab.job(minute= get_current_pet_minutes(), hour= get_current_pet_hours())
 def my_scheduled_job():
-  print ('hej')
-   #GPIO.output(20, GPIO.HIGH)
-   #sleep(3)
-   #GPIO.output(20, GPIO.LOW)
+  b = get_blue_value()
+  print ("bb=",b)
+  if GPIO.input(25) == 0 and b == "1" : #Jezeli w zrobioniku jest karma
+    pwm.start(0)
+    try:
+      GPIO.output(20, GPIO.HIGH) #Zapal diode zielona
+      pwm.ChangeDutyCycle(7.5) #wykonaj maly  obrot
+      sleep(1.5) #odczekaj 1.5 s
+      pwm.ChangeDutyCycle(11.5) #wykonaj  drugi  maly  obrot w tyl
+      sleep(1.5) #odczekaj  1.5 s
+      GPIO.output(20,GPIO.LOW) #Wylacz  diode  zielona
+      dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+      act = "The feed was automatically served"
+      name = get_current_pet()
+      with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO report (date,action,pet) VALUES (?,?,?)",(dateTime,act,name) )
+        con.commit()
+    except KeyboardInterrupt:
+      pwm.stop()
+      GPIO.cleanup()
+    return 'ok'
+  else:
+    return 'pusto'
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- backend/sqlite.py
+++ backend/sqlite.py
@@ -5,6 +5,11 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print "Opened database successfully";
 
-conn.execute('CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, type TEXT, portion INTEGER, hours CHAR, minutes CHAR, active INTEGER)')
+conn.execute('CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, type TEXT, portion INTEGER, hours CHAR, minutes CHAR, active INTEGER, user TEXT)')
+conn.execute('CREATE TABLE report (id INTEGER PRIMARY KEY, date TEXT, action TEXT, pet TEXT, user TEXT)')
+conn.execute('CREATE TABLE event (id INTEGER PRIMARY KEY, blue INTEGER, user TEXT)')
+conn.execute('CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, password TEXT, q1 TEXT)')
+#q1 - What's the name of your favorite pet?
+#conn.execute('CREATE TABLE keys (id INTEGER PRIMARY KEY, key TEXT, use INTEGER)')
 print "Table created successfully";
 conn.close()
